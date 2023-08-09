85ffaadb81a8a66f0e94d4316d2703b4ea9a3cb8
Aayush Gupta <aayushh@amazon.com>
2023-08-10 02:30:52 +0530

    add update APIs

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,10 @@ const Task = sequelize.define('Task', {
   title: {
     type: DataTypes.STRING,
     allowNull: false
+  },
+  taskStatus: {    
+    type: DataTypes.STRING,
+    allowNull: false
   }
 });
 
@@ -42,12 +46,38 @@ app.post('/lists', async (req, res) => {
   res.json(newList);
 });
 
-app.post('/tasks', async (req, res) => {
-  const { title, listId } = req.body;
-  const newTask = await Task.create({ title, ListId: listId });
-  res.json(newTask);
+app.post('/create/task', async (req, res) => {
+  try {
+    const { title, listId, taskStatus } = req.body;
+    const newTask = await Task.create({ title, ListId: listId, taskStatus });
+    res.json(newTask);
+  } catch(err) {
+    console.log("LOG ERROR: err", err);
+  }
 });
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.post('/update/task/taskStatus', async (req, res) => {
+  try {
+
+    const { id, taskStatus } = req.body;
+    const newTask = await Task.update({ taskStatus }, { where: { id } });
+    res.json(newTask);
+  } catch(err) {
+    console.log("LOG ERROR: err", err);
+  }
+});
+
+app.post('/update/task/listId', async (req, res) => {
+  try {
+    console.log("LOG INFO: req", req);
+    const { id, listId } = req.body;
+    const newTask = await Task.update({ ListId: listId }, { where: { id } });
+    res.json(newTask);
+  } catch(err) {
+    console.log("LOG ERROR: err", err);
+  }
+});
+
+app.listen(5001, () => {
+  console.log('Server is running on port 5001');
 });
